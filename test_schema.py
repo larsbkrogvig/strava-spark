43f1bb38a65d3ee7bb1025762190fdc7f1a38174
larsbkrogvig <larsbkrogvig@gmail.com>
2016-01-30 17:07:36 +0100

    First standalone pyspark application

--- test_schema.py
+++ test_schema.py
@@ -1,10 +1,20 @@
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
+# Configure application
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("Strava analyze"))
+
+# Initialize contexts
+sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
+# Read data
 df = sqlContext.read.format('com.databricks.spark.xml') \
         .options(rowTag='trkpt') \
         .load('activity.xml')
 
+# Show schema
 df.printSchema()
 
