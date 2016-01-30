3617f06fa008201aa3664713ef3961234d4d90f5
larsbkrogvig <larsbkrogvig@gmail.com>
2016-01-30 21:13:09 +0100

    Read file from s3

--- test_schema.py
+++ test_schema.py
@@ -1,3 +1,4 @@
+from csv import DictReader
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
@@ -10,10 +11,21 @@ conf = (SparkConf()
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
+# Read credentials
+with open('s3credentials.csv') as csvfile:
+    reader = DictReader(csvfile, delimiter=',')
+    row = next(reader, None)
+    s3AccessKeyId = row['Access Key Id']
+    s3SecretAccessKey = row['Secret Access Key']
+
+# Configure s3 credentials
+sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", s3AccessKeyId)
+sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", s3SecretAccessKey)
+
 # Read data
 df = sqlContext.read.format('com.databricks.spark.xml') \
         .options(rowTag='trkpt') \
-        .load('activity.xml')
+        .load('s3n://larsbk/strava-activities/lkrogvig/20140824-075956-Ride.gpx')
 
 # Show schema
 df.printSchema()
