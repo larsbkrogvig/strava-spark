9db850e0a9ff96f7bb824c192648d44cb5ed0843
larsbkrogvig <larsbkrogvig@gmail.com>
2016-01-31 22:45:41 +0100

    aws spark-submit now works

--- test_schema.py
+++ test_schema.py
@@ -2,30 +2,37 @@ from csv import DictReader
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
-# Configure application
-conf = (SparkConf()
-         .setMaster("local")
-         .setAppName("Strava analyze"))
+mode = 's3'
+#mode = 'local'
 
+# Set root file path
+filepath = {'s3': 's3n://larsbk/', 'local': '.'}
+FILE_PATH = filepath[mode]
+
+# Configure application
+conf = (SparkConf().setAppName("Strava analyze"))
+         #.setMaster("local")
+         
 # Initialize contexts
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
 # Read credentials
-with open('s3credentials.csv') as csvfile:
-    reader = DictReader(csvfile, delimiter=',')
-    row = next(reader, None)
-    s3AccessKeyId = row['Access Key Id']
-    s3SecretAccessKey = row['Secret Access Key']
+#with open('s3credentials.csv') as csvfile:
+#    reader = DictReader(csvfile, delimiter=',')
+#    row = next(reader, None)
+#    s3AccessKeyId = row['Access Key Id']
+#    s3SecretAccessKey = row['Secret Access Key']
 
 # Configure s3 credentials
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", s3AccessKeyId)
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", s3SecretAccessKey)
+#if mode == 's3':
+#    sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", s3AccessKeyId)
+#    sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", s3SecretAccessKey)
 
 # Read data
 df = sqlContext.read.format('com.databricks.spark.xml') \
         .options(rowTag='trkpt') \
-        .load('s3n://larsbk/strava-activities/lkrogvig/20140824-075956-Ride.gpx')
+        .load('%s/strava-activities/lkrogvig/20140824-075956-Ride.gpx' % FILE_PATH) # 
 
 # Show schema
 df.printSchema()
