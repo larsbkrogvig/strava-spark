48f91f48c45dc7ce16a3f78b18532f45fd7bf169
larsbkrogvig <larsbkrogvig@gmail.com>
2016-02-09 23:05:15 +0100

    Update bootstrap actions with boto3 and git

---
+++ bootstrap/github-wget.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/classes.py
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/analysis.py
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/schema.p
+
