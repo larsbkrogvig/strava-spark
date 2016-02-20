43b0201d4a63262a7a48163c6cb359aeac048dd1
larsbkrogvig <larsbkrogvig@gmail.com>
2016-02-20 15:12:22 +0100

    Adjust again

--- bootstrap/set-environment-variables.sh
+++ bootstrap/set-environment-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
+echo '' >> /home/hadoop/.bashrc
+echo 'export SPARK_HOME=/usr/lib/spark/' >> /home/hadoop/.bashrc
 echo 'export PYSPARK_SUBMIT_ARGS="--jars /home/hadoop/spark-xml_2.10-0.3.2.jar"' >> /home/hadoop/.bashrc
 fi
 
