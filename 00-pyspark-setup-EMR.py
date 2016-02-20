a88d075d9236522da49a65f212a9332bee735090
larsbkrogvig <larsbkrogvig@gmail.com>
2016-02-20 15:12:22 +0100

    Adjust again

--- 00-pyspark-setup-EMR.py
+++ 00-pyspark-setup-EMR.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-spark_home = '/usr/lib/spark/' # SPARK_HOME not set on EMR
+spark_home = os.environ.get('SPARK_HOME', None)
 
-pyspark_submit_args = '--jars /home/hadoop/spark-xml_2.10-0.3.2.jar'
-
+pyspark_submit_args = os.environ.get("PYSPARK_SUBMIT_ARGS", "")
 if not "pyspark-shell" in pyspark_submit_args: pyspark_submit_args += " pyspark-shell"
 os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
 
