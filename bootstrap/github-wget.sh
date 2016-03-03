a0148cb22773150b980d665ebd494195d76a1c7c
larsbkrogvig <larsbkrogvig@gmail.com>
2016-03-03 22:37:01 +0100

    Update path in bootstrap action

--- bootstrap/github-wget.sh
+++ bootstrap/github-wget.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
-wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/classes.py -P /home/hadoop/IPythonNB/
-wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/analysis.py -P /home/hadoop/IPythonNB/
-wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/schema.p -P /home/hadoop/IPythonNB/
-wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/00-pyspark-setup-EMR.py -P /home/hadoop/
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/py/classes.py -P /home/hadoop/IPythonNB/
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/py/analysis.py -P /home/hadoop/IPythonNB/
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/py/schema.p -P /home/hadoop/IPythonNB/
+wget https://raw.githubusercontent.com/larsbkrogvig/strava-spark/master/config/00-pyspark-setup-EMR.py -P /home/hadoop/
 fi
 
 
