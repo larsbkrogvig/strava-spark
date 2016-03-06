2f605625a5bac43462da2063b17ff5c171f94917
larsbkrogvig <larsbkrogvig@gmail.com>
2016-03-06 16:27:44 +0100

    Bootstrap actions for pandas

--- bootstrap/install-ipython-notebook-pyspark.sh
+++ bootstrap/install-ipython-notebook-pyspark.sh
@@ -5,17 +5,27 @@ set -x -e
 if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
 cd /home/hadoop
+sudo yum -y install git-all
 sudo pip install virtualenv
-#mkdir IPythonNB # I already did this in a previous bootstrap action
-cd IPythonNB
+
+# Clone strava-spark reop
+git clone https://github.com/larsbkrogvig/strava-spark.git 
+
+cd strava-spark/py
 /usr/local/bin/virtualenv -p /usr/bin/python2.7 venv
 source venv/bin/activate
 
 #Install ipython and dependency
 pip install "ipython[notebook]"
-pip install requests numpy pandas
+pip install requests numpy cython boto3
 pip install matplotlib
-pip install boto3
+
+#Install pandas
+git clone https://github.com/pydata/pandas.git
+cd pandas
+python setup.py install
+python setup.py build_ext --inplace
+cd ..
 
 #Create profile 	
 ipython profile create default
@@ -27,10 +37,7 @@ echo "c.NotebookApp.open_browser = False"  >>  /home/hadoop/.ipython/profile_def
 echo "c.NotebookApp.port = 8192" >>  /home/hadoop/.ipython/profile_default/ipython_notebook_config.py
 
 # Place downloaded pyspark conifg in correct directory
-mv /home/hadoop/00-pyspark-setup-EMR.py /home/hadoop/.ipython/profile_default/startup/00-default-setup.py
-
-echo "sh SPARK_HOME = "$SPARK_HOME
-echo "sh PYSPARK_SUBMIT_ARGS = "$PYSPARK_SUBMIT_ARGS
+mv /home/hadoop/strava-spark/config/00-pyspark-setup-EMR.py /home/hadoop/.ipython/profile_default/startup/00-default-setup.py
 
 nohup ipython notebook --no-browser > /mnt/var/log/python_notebook.log &
 fi
