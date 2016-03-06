ff867d49bd7a47f26a5e0682d0e3429020ec7d28
larsbkrogvig <larsbkrogvig@gmail.com>
2016-03-06 17:12:04 +0100

    Clean up old bootstrap actions

--- bootstrap/bootstrap-2-environment-variables.sh
+++ bootstrap/bootstrap-2-environment-variables.sh
@@ -4,5 +4,7 @@ then
 echo '' >> /home/hadoop/.bashrc
 echo 'export SPARK_HOME=/usr/lib/spark/' >> /home/hadoop/.bashrc
 echo 'export PYSPARK_SUBMIT_ARGS="--jars /home/hadoop/spark-xml_2.10-0.3.2.jar"' >> /home/hadoop/.bashrc
+echo 'export LC_ALL=en_US.UTF-8' >> /home/hadoop/.bashrc
+echo 'export LANG=en_US.UTF-8' >> /home/hadoop/.bashrc
 fi
 
--- bootstrap/github-install-clone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if grep isMaster /mnt/var/lib/info/instance.json | grep true;
-then
-
-fi
-
-
